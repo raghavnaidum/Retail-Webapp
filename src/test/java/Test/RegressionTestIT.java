e34d99a7033efbaacdfe94445b3e53ba5dac4570
SudhirG85 <prgsudhir@gmail.com>
2017-07-26 13:00:01 +0530

    Update RegressionTestIT.java

--- src/test/java/Test/RegressionTestIT.java
+++ src/test/java/Test/RegressionTestIT.java
@@ -21,7 +21,8 @@ public class RegressionTestIT {
 	 // WebDriver	driver = new FirefoxDriver();
 	 WebDriver	driver = new HtmlUnitDriver();
 	  //driver.get("http://localhost:8092/retailone/");
-	  driver.get("https://devopsdemoapp.cfapps.io/");
+	  //driver.get("https://devopsdemoapp.cfapps.io/");
+	  driver.get("https://RetailDemoPcfApp.cogpcfdevops.com/");
 	  Assert.assertEquals("Retail Application Demo", driver.getTitle());
 	  
 	  loginPage loginpage = new loginPage(driver);
